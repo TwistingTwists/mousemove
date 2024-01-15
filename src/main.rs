5780e67b1b887a2a5a6b0dd3027a8317f3a7bf52
Abhishek Tripathi <abhishek.tripathi456@gmail.com>
2024-01-15 20:56:10 +0530

    random offset with mouse.
    cross compilation does not work yet

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use autopilot::geometry::Point;
 use std::thread;
 use std::time::Duration;
+use rand::Rng;
 
 fn main() {
     // Set the screen resolution (change these values according to your screen)
@@ -11,12 +12,24 @@ fn main() {
     let speed = 50.0;
 
     loop {
+            // Set the range for random values
+    let random_range = 300;
+
+    let mut rng = rand::thread_rng();
+
         // Get the current mouse position
         let current_position = autopilot::mouse::location();
 
+
+
+        // Calculate the new position with added randomness
+        let random_offset_x = rng.gen_range(-random_range..=random_range) as f64;
+        let random_offset_y = rng.gen_range(-random_range..=random_range)as f64 ;
+
+
         // Calculate the new position (you can modify this according to your needs)
-        let new_x = (current_position.x + speed)as i32 % screen_width;
-        let new_y = (current_position.y + speed) as i32 % screen_height;
+        let new_x = (current_position.x + speed + random_offset_x)as i32 % screen_width;
+        let new_y = (current_position.y + speed + random_offset_y) as i32 % screen_height;
 
         // Move the mouse to the new position
         autopilot::mouse::move_to(Point::new(new_x as f64, new_y as f64));
