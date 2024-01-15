a202ebba89ca4511b56bf238f8fb46440ab8bbf8
Abhishek Tripathi <abhishek.tripathi456@gmail.com>
2024-01-15 23:29:56 +0530

    fix

--- scripts/manage_release.sh
+++ scripts/manage_release.sh
@@ -19,5 +19,22 @@ create_release(){
   -H "Authorization: Bearer $GH_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/$OWNER/$REPO/releases \
-  -d '{"tag_name":"v1.0.0","target_commitish":"master","name":"v1.0.0","body":"Description of the release","draft":false,"prerelease":false,"generate_release_notes":false}'
+  -d '{"tag_name":"v0.2.1","target_commitish":"main","name":"v0.2.1","body":"for windows pre compiled","draft":false,"prerelease":false,"generate_release_notes":false}'
 }
+
+upload_release_asset(){ 
+curl -L \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GH_TOKEN" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  -H "Content-Type: application/octet-stream" \
+  "https://uploads.github.com/repos/$OWNER/$REPO/releases/$RELEASE_ID/assets?name=mousemove.exe" \
+  --data-binary "@$BINARY_FILE"
+
+}
+
+# create_release <<< this will return a release_id like --- 137102270
+export RELEASE_ID=137102270
+# export BINARY_FILE=target/x86_64-pc-windows-gnu/release/mousemove.exe
+upload_release_asset # use RELEASE_ID here.
