cc11fb19e7830034ac6d17961bdb7b1dbff7ecb2
Abhishek Tripathi <abhishek.tripathi456@gmail.com>
2024-01-21 22:09:18 +0530

    cargo dist

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use autopilot::geometry::Point;
+use rand::Rng;
 use std::thread;
 use std::time::Duration;
-use rand::Rng;
 
 fn main() {
     // Set the screen resolution (change these values according to your screen)
@@ -12,23 +12,20 @@ fn main() {
     let speed = 50.0;
 
     loop {
-            // Set the range for random values
-    let random_range = 300;
+        // Set the range for random values
+        let random_range = 300;
 
-    let mut rng = rand::thread_rng();
+        let mut rng = rand::thread_rng();
 
         // Get the current mouse position
         let current_position = autopilot::mouse::location();
 
-
-
         // Calculate the new position with added randomness
         let random_offset_x = rng.gen_range(-random_range..=random_range) as f64;
-        let random_offset_y = rng.gen_range(-random_range..=random_range)as f64 ;
-
+        let random_offset_y = rng.gen_range(-random_range..=random_range) as f64;
 
         // Calculate the new position (you can modify this according to your needs)
-        let new_x = (current_position.x + speed + random_offset_x)as i32 % screen_width;
+        let new_x = (current_position.x + speed + random_offset_x) as i32 % screen_width;
         let new_y = (current_position.y + speed + random_offset_y) as i32 % screen_height;
 
         // Move the mouse to the new position
