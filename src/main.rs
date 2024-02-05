e5d17cbf28105026b4ca79e8ef00819bd3095588
Abhishek Tripathi <abhishek.tripathi456@gmail.com>
2024-02-05 18:13:32 +0530

    002 will this work?
    why not?

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,18 @@
-// use rdev::{listen, Event, EventType::MouseMove};
+// runtime = exectuor + reactor
+// futures = executor + _______  = 1/2 runtime
+// https://github.com/richardanaya/executor
+// exectuor = only executor
 
-use std::sync::mpsc;
-use std::thread;
-use std::time::Duration;
-
-#[derive(Debug)]
-enum Command {
-    Move,
-    Still,
-    ResetTimer,
+async fn say_hello() {
+    println!("async hello");
 }
 
-// park a thread ? using atomics, other shared state methods
-
 fn main() {
-    let (tx, rx) = mpsc::channel::<Command>();
-    let handle = thread::spawn(move || {
-        while let Ok(command) = rx.recv() {
-            match command {
-                Command::Move => println!("Move Please!"),
-                // random_move_loop();
-                Command::Still => println!("Stop Moving Please!"),
-                Command::ResetTimer => println!("Reset the timer to 5 min!"),
-            }
-        }
-    });
-    // handle.join().unwrap();
-    loop {
-        tx.send(Command::Move).unwrap();
-        thread::sleep(Duration::from_millis(300));
-        tx.send(Command::ResetTimer).unwrap();
-        thread::sleep(Duration::from_millis(300));
-
-        tx.send(Command::Still).unwrap();
-        thread::sleep(Duration::from_millis(300));
-    }
+    // say_hello(); // future => executor + ______ ?
+    // say_hello().await;
+    // await needs executor and reactor (the runtime).
+    // you cannot run asynchronous code in sync code. => you need a runtime for it.
+    // BUT if you need to run an async code in  a sync code in an async function ?
+    //  => need nested runtimes. Nopes. not allowed.
+    // async <- sync <- async
 }
