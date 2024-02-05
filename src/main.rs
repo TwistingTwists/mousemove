296ca93274e4af42bed415bed669b6e9f1e62cf1
Abhishek Tripathi <abhishek.tripathi456@gmail.com>
2024-02-05 22:07:04 +0530

    005 mouse moves!

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
-use std::time::Duration;
+#[allow(unused_mut)]
+mod random_move;
+use random_move::random_mouse_move;
+use rdev::{listen, Event, EventType, Key::Space};
+use tokio::sync::mpsc;
 use tokio::task::spawn_blocking;
+use tokio::time::Duration;
 
 // runtime = exectuor + reactor
 // futures = executor + _______  = 1/2 runtime
 // https://github.com/richardanaya/executor
 // exectuor = only executor
 
-async fn hello_delay(task: u64, time: u64) {
-    println!("Started: {task}");
-    let _ = spawn_blocking(move || {
-        // tokio::time::sleep(Duration::from_millis(time)).await;
-        std::thread::sleep(Duration::from_millis(time));
-    })
-    .await;
-    println!("Finished: {task}");
+#[derive(Debug)]
+enum Command {
+    MoveMouse,
+    ResetTimer,
 }
 
 #[tokio::main]
@@ -26,10 +27,68 @@ async fn main() {
     //  => need nested runtimes. Nopes. not allowed.
     // async <- sync <- async
 
-    tokio::join!(
-        hello_delay(1, 1200),
-        hello_delay(2, 1900),
-        hello_delay(3, 2800),
-        hello_delay(4, 3900)
-    );
+    let (tx, mut rx) = mpsc::channel::<Command>(10);
+    let tx_outer = tx.clone();
+    tokio::spawn(async move {
+        // will take a thread from `hot threads` in thread pool that tokio manages
+        let tx2 = tx.clone();
+        let mut listened_key_mouse_event = spawn_blocking(move || {
+            println!("Spawned keyboard / mouse listener");
+            blocking_function(tx2)
+        });
+        dbg!(&listened_key_mouse_event);
+    });
+
+    // Timer task
+    let mut remaining_time = 5;
+    // let mut remaining_time = 5 * 60;
+    let mut timer = tokio::time::interval(Duration::from_secs(1));
+    println!("before tokio select! -> {} ", remaining_time);
+
+    // tokio::spawn(my_timer(remaining_time));
+    loop {
+        tokio::select! {
+        _ = timer.tick() => {
+            remaining_time -= 1;
+            println!("Remaining time: {} seconds", remaining_time);
+            if remaining_time <= 0 {
+                tx_outer.send(Command::MoveMouse).await.unwrap();
+                // break;
+            }
+        }
+        Some(command) = rx.recv() => {
+            // if let Ok(command_enum) = command {
+            match command {
+                Command::MoveMouse => {
+                    println!("What if mouse actually moved?");
+                    random_mouse_move()
+                }
+
+                Command::ResetTimer => {
+
+                    remaining_time = 5; // Reset the timer
+                    // remaining_time = 5 * 60; // Reset the timer
+                }
+            }
+        }
+        }
+    }
+}
+
+fn blocking_function(tx: mpsc::Sender<Command>) {
+    let val = listen(move |event: Event| match event.event_type {
+        EventType::MouseMove { x, y } => {
+            println!("MouseMove: {} {}", x, y);
+            // tokio::time::sleep(Duration::from_millis(900));
+            // tx.blocking_send(Command::MoveMouse).unwrap();
+        }
+        EventType::KeyPress(Space) => {
+            tx.blocking_send(Command::ResetTimer).unwrap();
+        }
+        other => {
+            println!("Got {:?} ", other);
+        }
+    });
+
+    dbg!(&val);
 }
